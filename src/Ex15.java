9b519ff17529c57a3e7d726d7ea6f06fb974370f
Pedro <pdrocagens@gmail.com>
2026-03-24 17:10:22 -0300

    feat(ex15): Consumo de calorias.

--- src/Ex15.java
+++ src/Ex15.java
@@ -1,15 +1,6 @@
-//Uma pessoa deseja controlar sua alimentação e definiu um limite diário de
-//2.000 calorias.
-//Crie um programa que:
-//Pergunte quantas calorias foram consumidas no dia
-//O sistema deve:
-//Calcular o percentual consumido em relação ao limite
-//Informar:
-//Se está dentro do limite
-//Ou quanto ultrapassou
-
 void main() {
     Scanner sc = new Scanner(System.in);
+
     IO.println("Olá! Bem vindo ao QuaCa! Você definiu um limite diarío de 2.000 calorias");
     IO.println("");
     IO.println("Quantas calorias foram consumidas hoje?");
